dd5be330656fcdb3b7b15e0fca861c1599e41f5b
Oluwatosin Olufowobi <fowobiy2k@yahoo.com>
2022-04-04 06:22:13 +0100

    http get request tested

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useEffect, useState } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [fixtures, setFixtures] = useState([]);
+  // const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+
+    const getResources = async () => {
+
+      await fetchFixtures();
+
+    }
+
+    getResources()
+
+  }, [])
+
+  // Fetch Fixtures
+  const fetchFixtures = async () => {
+
+
+    const res = await fetch('/allmatches')
+    const data = await res.json()
+    console.log(data)
+    setFixtures(data)
+
+    return data
+  }
+
   return (
-    <div className="App">
-      <h1>Welcome to Pioneers Football Academy</h1>
+
+    <div>
+      <h1>Welcome</h1>
+
+      {fixtures && fixtures.map(fixture => {
+        return <p key={fixture.id}>{fixture.date}</p >
+      })}
     </div>
+
   );
 }
 
